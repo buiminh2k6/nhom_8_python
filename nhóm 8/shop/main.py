81cfd455e83477505d57cea58cc3cb3887541088
buiminh2k6 <minhbuoi25022006@gmail.com>
2026-06-09 22:52:25 +0700

    Add files via upload

--- nhóm 8/shop/main.py
+++ nhóm 8/shop/main.py
@@ -2,17 +2,17 @@ import customtkinter as ctk
 from tkinter import ttk
 from quanlytk import Quanlytk
 
-ctk.set_appearance_mode("Dark")
+ctk.set_appearance_mode("Light")
 ctk.set_default_color_theme("blue")
 
 root = ctk.CTk()
 
 style = ttk.Style(root)
 style.theme_use("default")
-style.configure("Treeview", background="#2b2b2b", foreground="white", rowheight=25, fieldbackground="#2b2b2b", borderwidth=0)
-style.map('Treeview', background=[('selected', '#1f538d')])
-style.configure("Treeview.Heading", background="#565b5e", foreground="white", relief="flat", font=("Arial", 10, "bold"))
-style.map("Treeview.Heading", background=[('active', '#1f538d')])
+style.configure("Treeview", background="#ffffff", foreground="black", rowheight=25, fieldbackground="#ffffff", borderwidth=0)
+style.map('Treeview', background=[('selected', '#1f538d')], foreground=[('selected', 'white')])
+style.configure("Treeview.Heading", background="#e0e0e0", foreground="black", relief="flat", font=("Arial", 10, "bold"))
+style.map("Treeview.Heading", background=[('active', '#c0c0c0')])
 
 app = Quanlytk(root)
 root.mainloop()
